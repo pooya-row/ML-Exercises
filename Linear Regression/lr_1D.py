bc4a2fb483f58e790137895002da2f5646acfd1e
pooya <pooya.rowghanian@gmail.com>
2020-09-01 11:07:17 -0400

    added R^2

--- Linear Regression/lr_1D.py
+++ Linear Regression/lr_1D.py
@@ -13,20 +13,31 @@ for line in open('data_1d.csv'):
 X = np.array(X)
 Y = np.array(Y)
 
-# # plot array data
+# # plot raw data
 # plt.scatter(X, Y)
 # plt.show()
 
 # calculate a & b
 a = (X.dot(Y) - len(X) * X.mean() * Y.mean()) / (X.dot(X) - X.mean() ** 2 * len(X))
 b = Y.mean() - a * X.mean()
-# print(f'a = {a}')
-# print(f'b = {b}')
 
 # calculate predictions
 Y_hat = a * X + b
+
+# calculate R^2
+del1 = Y - Y.mean()
+del2 = Y - Y_hat
+R2 = 1 - del2.dot(del2) / del1.dot(del1)
+# # alternative method
+# TSS = sum((Y - Y.mean()) ** 2)
+# RSS = sum((Y - Y_hat) ** 2)
+# R2 = 1 - RSS / TSS
 
 # plot data and fitted line
 plt.scatter(X, Y)
 plt.plot(X, Y_hat, c='red')
 plt.show()
+
+print(f'a = {a}')
+print(f'b = {b}')
+print(f'R^2 = {R2}')
