95de40cdb6f5763bb40b7132122a8655626db457
pooya <pooya.rowghanian@gmail.com>
2020-09-14 12:09:03 -0400

    array comprehension error fixed

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -1,5 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from sortedcontainers import SortedList
 from util import get_data
 
+
+class KNN:
+    def __init__(self, k):
+        self.k = k
+
+
+if __name__ == '__main__':
+    X, Y = get_data(2000)
+    print(X[1, :])
--- KNN/util.py
+++ KNN/util.py
@@ -4,10 +4,12 @@ import pandas as pd
 
 def get_data(limit=None):
     df = pd.read_csv('mnist_train.csv')
-    data = df.as_matrix()
+    data = df.to_numpy()
+    # print(data.head)
     np.random.shuffle(data)
     x = data[:, 1:] / 255
     y = data[:, 0]
+    y = y.reshape((len(y), 1))
     if limit is not None:
-        x, y = x[:limit], y[:, limit]
+        x, y = x[:limit, :], y[:limit]
     return x, y
