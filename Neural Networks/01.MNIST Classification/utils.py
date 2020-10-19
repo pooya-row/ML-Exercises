693dd5b88572c12c0ee925b89efdab20ff477d6b
pooya <pooya.rowghanian@gmail.com>
2020-10-18 21:49:12 -0400

    module containing functions

---
+++ Neural Networks/01.MNIST Classification/utils.py
@@ -0,0 +1,105 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
+
+
+# scale the data
+def scale_data(x_train, x_test):
+    x_train = x_train / 255.
+    x_test = x_test / 255.
+    return x_train, x_test
+
+
+# reduce data
+def reduce_date(x, y, n=5000):
+    '''
+    This function takes in two lists keeps only the first n elements of them
+    :param x: list
+    :param y: list
+    :param n: int
+    :return: truncated x-list, truncated y-list
+    '''
+    return x[:n], y[:n]
+
+
+# build the model
+def get_model(num_layers, input_shape):
+    '''
+    This function builds a Sequential model according to the below specification:
+        1. A 2D convolutional layer with a 5x5 kernel and 4 filters; no padding; ReLU activation function
+        2. MaxPooling with 3x3 kernel
+        2. n 2D convolutional layers with 3x3 kernels and 8 filters; no padding; ReLU activation function
+        3. MaxPooling layers, one after each of above Conv2Ds with 3x3 window, and strides of 1 (total of n)
+        4. A flatten layer, which unrolls the input into a column tensor
+        5. A dense output layer with 10 units and the softmax activation function
+    :param input_shape: tuple (pixel, pixel, channel=1)
+    :param num_layers: int, number of layers
+    :return: Sequential model object
+    '''
+
+    # instantiate the model
+    model = tf.keras.models.Sequential()
+
+    # add layers
+    model.add(Conv2D(4, kernel_size=5, padding='valid', activation='relu', input_shape=input_shape))
+    model.add(MaxPooling2D((3, 3), strides=1))
+
+    for i in range(num_layers):
+        model.add(Conv2D(8, kernel_size=3, padding='valid', activation='relu'))
+        model.add(MaxPooling2D((3, 3), strides=1))
+
+    model.add(Flatten())
+    model.add(Dense(10, activation='softmax'))
+
+    return model
+
+
+# compile the model
+def compile_model(mdl):
+    '''
+    This function takes in the model returned from get_model function, and compiles it with
+        * SGD optimiser (with default settings)
+        * cross-entropy loss function
+        * accuracy as the only metric.
+    This function does not return anything; the model will be compiled in-place.
+
+    :param mdl: Sequential model from `get_model`
+    :return: None
+    '''
+    mdl.compile(optimizer='SGD', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+
+# train the model
+def train_model(mdl, images, labels, num_e, batch_size, call_backs):
+    '''
+    This function trains the model for `num_e` epochs on the `scaled_train_images` and train_labels
+    and returns the training history, as returned by `model.fit`.
+
+    :param mdl: Sequential model from `get_model`
+    :param images: numpy.ndarray (n_train, pixel, pixel, channel=1)
+    :param labels: numpy.ndarray (n_train, 1)
+    :param num_e: int number of epochs
+    :param batch_size: int, size of mini batches
+    :param call_backs: list of callbacks for model saving and early-stopping
+    :return: model.fit.history
+    '''
+    if call_backs == []:
+        history = mdl.fit(images, labels, epochs=num_e, batch_size=batch_size)
+    else:
+        history = mdl.fit(images, labels, epochs=num_e,
+                          batch_size=batch_size, callbacks=call_backs)
+    return history
+
+
+# evaluate the model
+def evaluate_model(mdl, images, labels):
+    '''
+    This function evaluates the model on the `scaled_test_images` and `test_labels`.
+    and returns a tuple (test_loss, test_accuracy).
+
+    :param mdl: Sequential model from 'get_model'
+    :param images: numpy.ndarray (n_test, pixel, pixel, channel=1)
+    :param labels: numpy.ndarray (n_test,1)
+    :return: tuple (test_loss, test_accuracy)
+    '''
+    test_loss, test_accuracy = mdl.evaluate(images, labels)
+    return test_loss, test_accuracy
