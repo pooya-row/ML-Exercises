05948342e0d89504d01386c92aa5f9fecba04435
pooya <pooya.rowghanian@gmail.com>
2020-11-09 16:15:14 -0500

    visualized decision tree

--- Iris/iris_v00_skl_dt.py
+++ Iris/iris_v00_skl_dt.py
@@ -4,16 +4,19 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
+from sklearn import tree
 
 data = pd.read_csv('00_data/iris.data',
                    names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class'])
+
+print(data.head())
 
 # split data
 X_train, X_test, y_train, y_test = train_test_split(data.drop('class', axis=1),
                                                     data['class'], test_size=0.2)
 
 # build a tree model
-model = DecisionTreeClassifier()
+model = DecisionTreeClassifier(random_state=0, max_depth=2, splitter='best')#, min_samples_split=2)
 
 # train the model
 model.fit(X_train, y_train)
@@ -25,5 +28,10 @@ y_pred = model.predict(X_test)
 print(classification_report(y_test, y_pred))
 
 # visualize
-sns.pairplot(data, hue='class')
+# tree.plot_tree(model)
+feature_name = ['Sepal L', 'Sepal W', 'Petal L', 'Petal W']
+class_name = ['Setosa', 'Versicolor', 'Virginica']
+
+fig, axes = plt.subplots(nrows=1, ncols=1, figsize=(10, 8))
+tree.plot_tree(model, feature_names=feature_name, class_names=class_name, filled=True)
 plt.show()
